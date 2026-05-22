dbde59ff6c0ec8ddaef4e03effd9c0a82ef52859
Samandar <samandarmamurov0409@gmail.com>
2026-05-22 23:16:38 +0500

    js-5-oy-1-dars

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log(document.getElementById("text"));
+// console.log(document.getElementsByClassName("title"));
+// console.log(document.getElementsByTagName("text-p"));
+
+// const titleEl = document.querySelector (".text-p")
+// console.log(titleEl.textContent);
+// titleEl.style = `
+// color: blue;
+// border: 2px solid red;
+// `
+
+const randomBtnEl = document.querySelector("#random-btn");
+const body = document.body;
+
+randomBtnEl.style = `
+padding:10px 15px;
+border: 2px solid red;
+border-radius:10px;
+color:blue;
+cursor:pointer;
+
+`;
+function getRandomHex() {
+  const hexColor = "abcdef0123456789";
+  let hex = "#";
+  for (let i = 0; i < 6; i++) {
+    hex += hexColor[Math.trunc(Math.random() * 16)];
+  }
+  body.style = `background-color: ${hex}`;
+  document.querySelector("h1").textContent = hex; 
+}
+
